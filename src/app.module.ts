4a1677a47511d3bf734e91a26cfaa01c853cac3c
Tobin Chew <tobin@boffinsridge.co.uk>
2022-06-07 11:03:02 +0100

    working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
+import { ProxyModule } from '@finastra/nestjs-proxy';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ProxyModule.forRoot({
+      config: {},
+      services: [
+        {
+          id: 'revolut-merchant',
+          url: `https://merchant.revolut.com/api/1.0`,
+        },
+        {
+          id: 'revolut-merchant-sandbox',
+          url: `https://sandbox-merchant.revolut.com/api/1.0`,
+        },
+        {
+          id: 'revolut-business',
+          url: 'https://b2b.revolut.com/api/1.0',
+        },
+        {
+          id: 'revolut-business-sandbox',
+          url: 'https://sandbox-b2b.revolut.com/api/1.0',
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
